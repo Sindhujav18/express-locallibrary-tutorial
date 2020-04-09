5f1721de2ba0be28fce2acc3ca7a9f0c14db5963
Sindhu <sindhujav18@gmail.com>
2020-04-08 22:37:25 -0500

    committing changes

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const dev_db_url = 'mongodb+srv://dbUser:<Password>@cluster0-8awuk.mongodb.net/local_library?retryWrites=true&w=majority'
+const dev_db_url = 'mongodb+srv://dbUser:Password@cluster0-8awuk.mongodb.net/local_library?retryWrites=true&w=majority'
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true } ,{ useUnifiedTopology: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
