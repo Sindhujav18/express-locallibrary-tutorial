fd7049733d1b5c23e98a5c2218e197901c11ef4c
Sindhu <sindhujav18@gmail.com>
2020-04-23 23:52:07 -0500

    added views

--- app.js
+++ app.js
@@ -7,22 +7,26 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
+
 
 var app = express();
-dotenv.config({ path: '.env' });
-// Set up mongoose connection
+dotenv.config({ path: '.env' })
+
+
 const mongoose = require('mongoose');
 const dev_db_url = process.env.ATLAS_URI;
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true  ,   useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
